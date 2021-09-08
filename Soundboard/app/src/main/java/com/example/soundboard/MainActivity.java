1ec24efd1731c63ab0d5e66952e1cf881da7520a
Thomas Trombino <ttrombin@uci.edu>
2021-09-08 14:25:57 -0700

    Added remaining clips

--- Soundboard/app/src/main/java/com/example/soundboard/MainActivity.java
+++ Soundboard/app/src/main/java/com/example/soundboard/MainActivity.java
@@ -29,7 +29,39 @@ public class MainActivity extends AppCompatActivity {
 
         List<String> nameList = Arrays.asList(getResources().getStringArray(R.array.soundNames));
 
-        BoardObject[] boardItems = {new BoardObject(nameList.get(0), R.raw.airhorn), new BoardObject(nameList.get(1), R.raw.crash), new BoardObject(nameList.get(2), R.raw.easy) };
+        BoardObject[] boardItems = {
+                new BoardObject(nameList.get(0), R.raw.airhorn),
+                new BoardObject(nameList.get(1), R.raw.crash),
+                new BoardObject(nameList.get(2), R.raw.easy),
+                new BoardObject(nameList.get(3), R.raw.renner),
+                new BoardObject(nameList.get(4), R.raw.das),
+                new BoardObject(nameList.get(5), R.raw.newcard),
+                new BoardObject(nameList.get(6), R.raw.bone),
+                new BoardObject(nameList.get(7), R.raw.ropers),
+                new BoardObject(nameList.get(8), R.raw.young),
+                new BoardObject(nameList.get(9), R.raw.waiting),
+                new BoardObject(nameList.get(10), R.raw.price_is_right),
+                new BoardObject(nameList.get(11), R.raw.hall),
+                new BoardObject(nameList.get(12), R.raw.oates),
+                new BoardObject(nameList.get(13), R.raw.salud),
+                new BoardObject(nameList.get(14), R.raw.shinebox1),
+                new BoardObject(nameList.get(15), R.raw.anymore),
+                new BoardObject(nameList.get(16), R.raw.fresh),
+                new BoardObject(nameList.get(17), R.raw.shinebox2),
+                new BoardObject(nameList.get(18), R.raw.woltz),
+                new BoardObject(nameList.get(19), R.raw.fredo),
+                new BoardObject(nameList.get(20), R.raw.henderson),
+                new BoardObject(nameList.get(21), R.raw.billy),
+                new BoardObject(nameList.get(22), R.raw.odoyle1),
+                new BoardObject(nameList.get(23), R.raw.odoyle2),
+                new BoardObject(nameList.get(24), R.raw.odoyle3),
+                new BoardObject(nameList.get(25), R.raw.colonel_sanders),
+                new BoardObject(nameList.get(26), R.raw.snoop),
+                new BoardObject(nameList.get(27), R.raw.nate),
+                new BoardObject(nameList.get(28), R.raw.spooner1),
+                new BoardObject(nameList.get(29), R.raw.spooner2),
+
+        };
 
         boardList.addAll(Arrays.asList(boardItems));
 
